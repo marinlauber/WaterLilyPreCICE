0e87c99a208f10055abe6e544aa6b3faaf13528d
marinlauber <marinlauber@gmail.com>
2025-02-06 14:24:54 +0100

    moving mesh example

--- examples/ThreeD_MeshBody.jl
+++ examples/ThreeD_MeshBody.jl
@@ -2,8 +2,11 @@ using WaterLily,WaterLilyPreCICE,StaticArrays,WriteVTK
 
 function make_sphere(;L=32,Re=250,U=1)
     # move the geometry to the center of the domain
-    map(x,t) = x .- SA[L,L,L/2]
-    # map(x,t) = x .- SA[L,L+L/4*sin(2π*t/L),L/2] # this has a moving body
+    # map(x,t) = x .- SA[L,L,L/2]
+    map(x,t) = x .- SA[L,L+L/4*sin(2π*t/L),L/2] 
+    # @TODO this has a moving body if you comment this line
+    # you need to change the set the flag remeasure to true in the sim_step! function
+    
     # make the body from the stl mesh
     body = MeshBody(joinpath(@__DIR__,"../meshes/cube.stl");map,scale=L/2)
     # generate sim
@@ -27,7 +30,7 @@ t₀,duration,step = 0.,10,0.1
 # run the sim
 @time for tᵢ in range(t₀,t₀+duration;step)
     # update until time tᵢ in the background
-    sim_step!(sim,tᵢ;remeasure=false)
+    sim_step!(sim,tᵢ;remeasure=true)
     write!(wr,sim)
     # print time step
     println("tU/L=",round(tᵢ,digits=4),", Δt=",round(sim.flow.Δt[end],digits=3))
